089c738e66778c0e12e721cb906318460d50bfe3
Thiago <tjose.corp@gmail.com>
2025-10-17 19:14:55 -0300

    feat: implementação da classe Main com fluxo principal para criar e exibir pedidos a partir de entradas do usuário

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Client;
+import model.Order;
+import model.OrderItem;
+import model.Product;
+import model.enuns.OrderStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        String data = sc.nextLine();
+        LocalDate date = LocalDate.parse(data, formatter);
+        Client client = new Client(name, email, date);
+        System.out.print("Status: ");
+        String status = sc.next();
+        System.out.println("Enter order data: ");
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        Order order = new Order(client, dateTime, OrderStatus.valueOf(status.trim().toUpperCase()));
+        for (int i = 1; i <= n; i++) {
+            sc.nextLine();
+            System.out.printf("Enter #%d° item data:%n", i);
+            System.out.print("Product name: ");
+            String productName = sc.nextLine();
+            System.out.print("Product price: ");
+            Double productPrice = sc.nextDouble();
+            System.out.print("Quantity: ");
+            Integer quantity = sc.nextInt();
+
+            OrderItem orderItem = new OrderItem(productPrice, new Product(productName), quantity);
+            order.addItem(orderItem);
         }
+        System.out.println();
+        System.out.println(order);
     }
 }
